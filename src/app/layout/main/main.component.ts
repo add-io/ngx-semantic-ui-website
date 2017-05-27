ae44193df71fa4edd52b1feeac510afa75409bef
Terry <tspayne87@gmail.com>
2017-05-27 10:49:23 -0700

    Adding in some changes to deal with mobile devices.

--- src/app/layout/main/main.component.ts
+++ src/app/layout/main/main.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Renderer2 } from '@angular/core';
+import { TransitionService } from "ngx-semantic-ui";
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  private _transition: TransitionService;
+
+  showMenu: boolean = false;
+
+  @ViewChild("mainMenu", { read: ElementRef }) menu: ElementRef;
+
+  constructor(private _renderer: Renderer2) {
+    this._transition = new TransitionService(this._renderer);
+  }
 
   ngOnInit() {
   }
 
+  triggerShowMenu() {
+    if (!this.showMenu) {
+      this.showMenu = true;
+      this._transition.animate(this.menu.nativeElement, "fade");
+    }
+  }
+
+  triggerHideMenu() {
+    if (this.showMenu) {
+      this._transition.animate(this.menu.nativeElement, "fade", 200, "out").then(() => {
+        this.showMenu = false;
+      });
+    }
+  }
 }
